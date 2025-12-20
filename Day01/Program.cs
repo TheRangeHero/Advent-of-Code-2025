3fd484b1cdd8b6d842ec138772d20f316b9c92cf
Mihail Stanchev <mihail.stanchev97@gmail.com>
2025-12-21 01:43:12 +0200

    Day01_Task1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace Day01
+﻿using System.Runtime.CompilerServices;
+
+namespace Day01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            InputReader reader = new InputReader();
+
+            var input = reader.ReadLines("input.txt");
+            var solver = new Solver();
+            int task1 = solver.SolverTask1(input);
+
+        }
+    }
+
+
+    internal class InputReader
+    {
+        public string[] ReadLines(string fileName)
+        {
+            return File.ReadAllLines(fileName);
+        }
+    }
+
+    internal class Solver
+    {
+        public int SolverTask1(string[] input)
+        {
+            int result = 0;
+            int startPosition = 50;
+
+            char direction;
+            int dialNum;
+
+            foreach (string line in input)
+            {
+                direction = (line[0]);
+                dialNum = int.Parse(line.Substring(1));
+
+                switch (direction)
+                {
+                    case 'L':
+                        startPosition -= dialNum;
+                        break;
+                    case 'R':
+                        startPosition += dialNum;
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
+
+            return 0;
         }
     }
 }
