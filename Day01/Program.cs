1f9b103873a3978bbe303730206caefa95ee9a1f
Mihail Stanchev <108618829+TheRangeHero@users.noreply.github.com>
2025-12-24 22:37:21 +0200

    Refactored the Project architecture
    This reverts commit 144d0828a1079b4d4ec879927d85c12ba6081bd5.

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -54,6 +54,7 @@ namespace Day01
                             {
                                 startPosition = 0;
                             }
+
                         }
                         break;
                     case 'R':
